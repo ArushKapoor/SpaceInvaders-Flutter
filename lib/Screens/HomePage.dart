e34d6b8d820fea7e22fae57127338618773c92f6
ArushKapoor <arush0113.cse19@chitkara.edu.in>
2020-10-09 11:41:05 +0530

    added basic structure

--- lib/Screens/HomePage.dart
+++ lib/Screens/HomePage.dart
@@ -9,8 +9,59 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final _height = MediaQuery.of(context).size.height;
+    // print(MediaQuery.of(context).devicePixelRatio);
+    final _width = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      backgroundColor: Colors.teal,
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: GridView.builder(
+                  itemCount: 540,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 20),
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(3.5)),
+                          color: Colors.grey[900],
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: _height * 0.05),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text(
+                    'Play',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  Text(
+                    'Left',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  Text(
+                    'Up',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  Text(
+                    'Right',
+                    style: TextStyle(color: Colors.white),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
